554dcd75c8abe45502459b5d76dd4bb2b7819f22
idan <idanelbaz@gmail.com>
2019-06-20 18:02:48 +0300

    kjhghg

--- js/apps/mail-app/pages/imail-main.js
+++ js/apps/mail-app/pages/imail-main.js
@@ -9,19 +9,11 @@ export default {
         console.log('App has been Mounted!');
     },
     template: `
-<<<<<<< HEAD
-      <div>
-    <app-header></app-header>
-    <app-side-bar></app-side-bar>
-    <app-list></app-list>
-    </div>
-=======
         <div class="imail-main">
             <app-header></app-header>
             <app-side-bar></app-side-bar>
             <app-list></app-list>
         </div>
->>>>>>> 4d79bb58f1e04ae24fce48f7727e0455f61c80ce
     `,
     components: {
         appHeader,
