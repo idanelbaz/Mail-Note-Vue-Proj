f2dde97ac471597206cba7f2666e999d59743363
Daniel Shokri <danielshokri@gmail.com>
2019-06-21 10:21:57 +0300

    yes

--- js/apps/mail-app/cmp/app-side-bar.cmp.js
+++ js/apps/mail-app/cmp/app-side-bar.cmp.js
@@ -3,11 +3,11 @@ export default {
     template: `
             <section class="app-side-bar">
                 <nav>
-                    <router-link  to="/imail">Inbox</router-link><br>
+                    <router-link  to="/imail"><i class="el-icon-message"></i>Inbox</router-link><br>
                     <router-link to="/favorites">Favorites</router-link><br>
                     <router-link to="/trash">Trash</router-link><br>
                     <router-link to="/sent-mail">Sent mail</router-link> 
-                    <router-link to="/compose">Conpose</router-link> 
+                    <router-link to="/compose"><i class="el-icon-position"></i> New Mail</router-link> 
                 </nav>
                 <p>unread status</p>
             </section>    
