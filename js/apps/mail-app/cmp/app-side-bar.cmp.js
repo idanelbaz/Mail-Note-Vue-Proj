cb24844744b56fcd8f9aca6df76b20178c46b590
Daniel Shokri <danielshokri@gmail.com>
2019-06-21 17:37:37 +0300

    changes

--- js/apps/mail-app/cmp/app-side-bar.cmp.js
+++ js/apps/mail-app/cmp/app-side-bar.cmp.js
@@ -4,9 +4,9 @@ export default {
             <section class="app-side-bar">
                 <nav>
                     <router-link  to="/imail"><i class="el-icon-message"></i>Inbox</router-link><br>
-                    <router-link to="/favorites">Favorites</router-link><br>
-                    <router-link to="/trash">Trash</router-link><br>
-                    <router-link to="/sent-mail">Sent mail</router-link> 
+                    <router-link to="/favorites"><i class="el-icon-star-off"></i>Favorites</router-link><br>
+                    <router-link to="/trash"><i class="el-icon-delete"></i>Trash</router-link><br>
+                    <router-link to="/sent-mail"><i class="el-icon-folder-opened"></i>Sent mail</router-link><br>
                     <router-link to="/compose"><i class="el-icon-position"></i> New Mail</router-link> 
                 </nav>
                 <p>unread status</p>
