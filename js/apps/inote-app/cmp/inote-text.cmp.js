5adea21d2e01bf1041e15af311b9e08849e6253f
idan <idanelbaz@gmail.com>
2019-06-23 11:56:52 +0300

    add-page

--- js/apps/inote-app/cmp/inote-text.cmp.js
+++ js/apps/inote-app/cmp/inote-text.cmp.js
@@ -1,14 +1,16 @@
+import eventBus from '../../../event-bus.js';
+
 export default {
     template: `
         <section class="inote-header">
-        <el-input  type="text" v-model="text" @enter="addNote" placeholder="What's on your mind..." clearable >
+        <el-input  type="text" v-model="note.text" @enter="addNote" placeholder="What on your mind..." clearable >
         </el-input>
         <div class="btn-input-cont">
-        <el-button class="el-icon-position"></el-button> 
-        <el-button  class="el-icon-edit-outline"></el-button>
-        <el-button class="el-icon-video-camera-solid"></el-button>
-        <el-button class="el-icon-picture-outline"></el-button>
-        <el-button class="el-icon-headset"></el-button>
+        <!-- <el-button class="el-icon-position"></el-button>  -->
+        <el-button @click="chooseTxt"  class="el-icon-edit-outline"></el-button>
+        <el-button  @click="chooseVideo" class="el-icon-video-camera-solid"></el-button>
+        <el-button @click="chooseImg" class="el-icon-picture-outline"></el-button>
+        <el-button @click="chooseSound" class="el-icon-headset"></el-button>
         <el-button @click.enter="addNote" type="success" class="add-note-btn" plain>Add</el-button>
         </div>
         
@@ -22,13 +24,36 @@ export default {
     `,
     data() {
         return {
-            text: null,
+            note: {
+                text: '',
+                type: '',
+            }
+
 
         }
     },
     methods: {
+
+        chooseTxt() {
+            this.note.text = 'What on your mind...';
+            this.note.type = 'txt';
+        },
+        chooseImg() {
+            this.note.text = 'Enter image URL ';
+            this.note.type = 'Img';
+        },
+        chooseSound() {
+            this.note.text = 'Enter sound URL ';
+            this.note.type = 'sound';
+        },
+        chooseVideo() {
+            this.text = 'Enter video URL ';
+            this.note.type = 'video';
+        },
+
+
         addNote() {
-            console.log('Added note')
+            eventBus.$emit('chooshedNote', this.note);
         }
     },
 }
