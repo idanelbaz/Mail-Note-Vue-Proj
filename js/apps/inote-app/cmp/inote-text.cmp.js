79339594e1a0075c6c957f4922f9890c85713464
idan <idanelbaz@gmail.com>
2019-06-23 11:09:01 +0300

    fix

--- js/apps/inote-app/cmp/inote-text.cmp.js
+++ js/apps/inote-app/cmp/inote-text.cmp.js
@@ -1,13 +1,30 @@
-export default{
+export default {
     template: `
         <section class="inote-header">
-        <el-input type="text" v-model="text" @enter="addNote" placeholder="What's on your mind..." clearable></el-input> 
+        <el-input  type="text" v-model="text" @enter="addNote" placeholder="What's on your mind..." clearable >
+        </el-input>
+        <div class="btn-input-cont">
+        <el-button class="btn-input" class="el-icon-position"></el-button> 
+        <el-button  class="btn-input" class="el-icon-edit-outline"></el-button>
+        <el-button class="btn-input" class="el-icon-video-camera-solid"></el-button>
+        <el-button class="btn-input" class="el-icon-picture-outline"></el-button>
+        <el-button class="btn-input" class="el-icon-headset"></el-button>
+        
+        </div>
         <el-button @click.enter="addNote" type="success" class="add-note-btn" plain>Add</el-button>
+        
+
+
         </section>
+
+  
+      
+
     `,
     data() {
         return {
-            text: null
+            text: null,
+
         }
     },
     methods: {
