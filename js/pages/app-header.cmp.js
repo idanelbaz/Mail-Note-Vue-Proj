04f61a898048aca9761d0e48778fdfc2e6bc59e7
Daniel Shokri <danielshokri@gmail.com>
2019-06-24 13:50:34 +0300

    feg

--- js/pages/app-header.cmp.js
+++ js/pages/app-header.cmp.js
@@ -28,48 +28,17 @@ export default {
                     <router-link class="header-nav" to="/imail">iMail</router-link>
                     </a>
 
+                        </div>
                     </div>
-                    </div>
-            </div>
+                </div>
             </div>
         </div>
     </nav>
-                <!-- <el-menu
-                :default-active="activeIndex"
-                        class="el-menu-demo"
-                        mode="horizontal"
-                        background-color="#545c64"
-                        text-color="#fff"
-                        active-text-color="#ffd04b">
-                <el-menu-item index="1">
-                <i class="el-icon-s-home"></i>
-                         <router-link class="header-nav" exact to="/">Home</router-link>
-                         </el-menu-item>
-                    
-                         <el-menu-item index="2">
-                         <i class="el-icon-message"></i>
-                         <router-link class="header-nav" to="/imail">iMail</router-link>
-                         </el-menu-item>
-
-                         <el-menu-item index="3">
-                         <i class="el-icon-notebook-1"></i>
-                        <router-link class="header-nav" to="/inote">iNote</router-link>
-                        </el-menu-item>
-
-                         <el-menu-item index="4">
-                        <router-link class="header-nav" to="/book">Book</router-link>
-                        </el-menu-item>
-                        </el-menu>
-                         -->
-            </header>    
+</header>    
     `,
     data() {
         return {
-            activeIndex: '1',
-            activeIndex2: '2',
+
         }
     },
-    components:{
-        
-    }
 }
--- js/pages/homepage.cmp.js
+++ js/pages/homepage.cmp.js
@@ -1,7 +1,16 @@
 export default {
     template: `
     <section class="home-page">
-        <img src="../imgs/bg.png" id="bg"/>
+        <h1 class="home-header">iSUS APP.</h1>
+        <div class="home-slogan"><span class="slogan-text">Notes And Mails For Daylily Usage</span></div>
+        <div class="icons-div">
+        <router-link class="header-nav" to="/inote">
+        <div class="inote-logo"><img src="imgs/inote-logo.png"/></div>
+        </router-link>
+        <router-link class="header-nav" to="/imail">
+        <div class="imail-logo"><img src="imgs/logo.png"/></div>
+    </router-link>
+        </div>
     </section>
     `
 }
