4f5e8ec2b04fd541bb6ba4c62001b042d67d1c54
Daniel Shokri <danielshokri@gmail.com>
2019-06-24 12:23:59 +0300

    added main logo

--- js/pages/app-header.cmp.js
+++ js/pages/app-header.cmp.js
@@ -2,7 +2,39 @@ export default {
     name: 'TheAppHeader',
     template: `
             <header>
-                <el-menu
+            <nav class="navbar" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                    <a class="navbar-item">
+                    <img src="imgs/app-logo.png" width="75" height="50">
+                    </a>
+
+                    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    </a>
+                </div>
+
+                <div id="navbarBasicExample" class="navbar-menu">
+                    <div class="navbar-start">
+                    <a class="navbar-item">
+                     <router-link class="header-nav" exact to="/">Home</router-link>
+                    </a>
+
+                    <a class="navbar-item">
+                    <router-link class="header-nav" to="/inote">iNote</router-link>
+                    </a>
+                    <a class="navbar-item">
+                    <router-link class="header-nav" to="/imail">iMail</router-link>
+                    </a>
+
+                    </div>
+                    </div>
+            </div>
+            </div>
+        </div>
+    </nav>
+                <!-- <el-menu
                 :default-active="activeIndex"
                         class="el-menu-demo"
                         mode="horizontal"
@@ -28,7 +60,7 @@ export default {
                         <router-link class="header-nav" to="/book">Book</router-link>
                         </el-menu-item>
                         </el-menu>
-                        
+                         -->
             </header>    
     `,
     data() {
